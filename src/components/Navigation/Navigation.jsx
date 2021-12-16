2fdb12167ee3f6e14664d2f5af727f9e5aa75fd6
Руслан Давлетшин <96132138+MrDrProfesor@users.noreply.github.com>
2021-12-16 23:08:02 +0400

    изменил главную

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,18 +2,21 @@ import React, {useContext} from 'react';
 import {Link, useNavigate} from "react-router-dom";
 import {AppContext} from "../../App";
 import '../../App.css';
+import Logo from '../logo.png'
 
 const Navigation = () => {
     const {isAuth, setIsAuth} = useContext(AppContext)
     let navigate = useNavigate();
     return (
         <header className='header'>
+            <img className="Logo" alt="главная страница" src={Logo}/>
+            <h1>Testcaser</h1>
             <nav className='links'>
-                {!isAuth && <button onClick={() => navigate(`/private`)}>Войти</button>}
-                {isAuth && <button onClick={() => setIsAuth(false)}>Выйти</button>}
+                {/*{!isAuth && <button onClick={() => navigate(`/private`)}>Войти</button>}*/}
+                {/*{isAuth && <button onClick={() => setIsAuth(false)}>Выйти</button>}*/}
                 <Link to="/">Main</Link>
-                <Link to="/tour">tour</Link>
-                <Link to="/private">private</Link>
+                {/*<Link to="/tour">tour</Link>*/}
+                {/*<Link to="/private">private</Link>*/}
             </nav>
         </header>
     );
